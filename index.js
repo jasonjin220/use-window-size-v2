cbdad62fed566a2a4029ee6ca1f49c1efd51eca5
jasonjin220 <jasonjin220@gmail.com>
2022-06-12 09:25:11 -0400

    downgrade react version

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { debounce } from "lodash.debounce";
+import debounce from "lodash.debounce";
 
 const getWindowDimensions = () => {
   const { innerWidth: width, innerHeight: height } = window;
